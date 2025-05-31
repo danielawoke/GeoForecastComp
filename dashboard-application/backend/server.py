b5a5cca942496722dd7efff0f72df6fae7a7dd04
Daniel Awoke <123602707+quackTheMackOfTheCracks@users.noreply.github.com>
2025-05-31 11:47:05 -0400

    bn

---
+++ dashboard-application/backend/server.py
@@ -0,0 +1,76 @@
+import strawberry
+import typing
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from strawberry.fastapi import GraphQLRouter
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# an object is made with this class, and we can mark it with strawberry as a type
+@strawberry.type
+class User:
+    name: str
+    word:int
+
+@strawberry.type
+class UserList:
+    userList: typing.List[User]
+
+lst = []
+for i in range(0,100):
+    lst.append(User(name = "okay"+str(i), word = i))
+
+# this function is the resolver, it is what we use to then return what value we need
+def allData() -> typing.List[User]:
+    return lst
+# This right here is is the selling point of graphQL, if you make these large, 
+# deeply nested data strucutres that are defined by the schema
+# you can easily make requests for the subdata contained inside of them with a request
+
+@strawberry.type
+class Query:
+    last_user: typing.List[User] = strawberry.field(resolver=allData)
+  
+
+
+schema = strawberry.Schema(Query)
+graphql_app = GraphQLRouter(schema)
+
+app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Define a route at the root web address ("/")
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+
+class Item(BaseModel):
+    name: str
+    
+
+
+@app.post("/postRequest/")
+async def create_item(item: Item):
+    uri = "mongodb+srv://dawoke:'Bluebear1?123'@cluster0.oyu906n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    return item.name
+
+
--- dashboard-application/src/app/page.tsx
+++ dashboard-application/src/app/page.tsx
@@ -1,13 +1,49 @@
+'use client'
+
 import Image from "next/image";
+import React, { useRef } from 'react';
 
 export default function Home() {
+  const textArea = useRef(null);
+  
+  async function f(){
+    let data = textArea.current.value
+    const url = 'http://127.0.0.1:8000/postRequest/';
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+                              "name":"stevie Wozinakerson"
+                          }),
+                        })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        console.log('Success:', responseData);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+  }
+
+  
+
   return (
     <div className="w-150 h-screen flex flex-col m-auto">
       <div>
         <div className="mt-10 font-[impact] text-5xl text-black w-150 drop-shadow-[0_0px_3px_rgba(255,255,255,1)] m-auto">
           Paste or upload your data
         </div>
-        <textarea className="text-black mt-10 w-150" style={{backgroundColor: 'rgba(255, 255, 255, 0.94)'}}></textarea>
+        <textarea className="text-black mt-10 w-150" style={{backgroundColor: 'rgba(255, 255, 255, 0.94)'}} ref={textArea} />
+         <button onClick={f} type="button">Click Me!</button> 
       </div>
     </div>
   );
